8c451e8c98bb3c2ef8f7f15f42ff5d6979e20e78
yangmr <759168442@qq.com>
2020-12-01 14:45:33 +0800

    安装了elment-ui和bootstrap

--- back-system/vue.config.js
+++ back-system/vue.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    //打包时的静态资源的路径
-    publicPath : "./",
-    //配置vue-cli的服务器
-    devServer : {
-        //自动打开浏览器
-        open : true,
-        //设置打开的端口号
-        port : 7777,
-        //关闭https协议
-        https : false,
-        //设置主机名
-        host : "localhost"
-    },
-    //关闭eslint
-    lintOnSave : false,
-    //设置打包时不生成.map后缀类型的文件,提高打包的构建速度
-    productionSourceMap : false
-}
+  //打包时的静态资源的路径
+  publicPath: "./",
+  //配置vue-cli的服务器
+  devServer: {
+    //自动打开浏览器
+    open: true,
+    //设置打开的端口号
+    port: 7777,
+    //关闭https协议
+    https: false,
+    //设置主机名
+    host: "localhost"
+  },
+  //关闭eslint
+  lintOnSave: false,
+  //设置打包时不生成.map后缀类型的文件,提高打包的构建速度
+  productionSourceMap: false
+};
